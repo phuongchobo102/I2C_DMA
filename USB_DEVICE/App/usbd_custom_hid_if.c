963944e280a27dc180e624b25d66680353b4aedd
Phu Tran <tranquocgiaphu@gmail.com>
2023-02-13 12:04:37 +0700

    -Minh modifies usbd_custom_hid_if.c:   +__ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DESC_SIZE] __ALIGN_END = { 		/* USER CODE BEGIN 0 */ 		0x06, 0x00, 0xff, // Usage Page(Undefined ) 		0x09, 0x01, // USAGE (Undefined) 		0xa1, 0x01, // COLLECTION (Application) 		0x15, 0x00, // LOGICAL_MINIMUM (0) 		0x26, 0xff, 0x00, // LOGICAL_MAXIMUM (255) 		0x75, 0x08, // REPORT_SIZE (8) 		0x95, 0x40, // REPORT_COUNT (64) 		0x09, 0x01, // USAGE (Undefined) 		0x81, 0x02, // INPUT (Data,Var,Abs) 		0x95, 0x40, // REPORT_COUNT (64) 		0x09, 0x01, // USAGE (Undefined) 		0x91, 0x02, // OUTPUT (Data,Var,Abs) 		0x95, 0x01, // REPORT_COUNT (1) 		0x09, 0x01, // USAGE (Undefined) 		0xb1, 0x02, // FEATURE (Data,Var,Abs) 		/* USER CODE END 0 */ 		0xC0 /* END_COLLECTION */ }; +static int8_t CUSTOM_HID_OutEvent_FS(uint8_t* state) //static int8_t CUSTOM_HID_OutEvent_FS  (uint8_t event_idx, uint8_t state) {   /* USER CODE BEGIN 6 */ 	memcpy(report_buffer, state, 64); 	USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, report_buffer, 0x40); 	flag_rx = 1;   return (USBD_OK);   /* USER CODE END 6 */ } -Minh modifies usbd_desc.c: +STMicroelectronics=> Nova -Minh modifies usbd_conf.h: +#define USBD_CUSTOMHID_OUTREPORT_BUF_SIZE     64 /*---------- -----------*/ #define USBD_CUSTOM_HID_REPORT_DESC_SIZE     33

--- USB_DEVICE/App/usbd_custom_hid_if.c
+++ USB_DEVICE/App/usbd_custom_hid_if.c
@@ -31,7 +31,9 @@
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
-
+//To extern the report_buffer variable
+extern uint8_t report_buffer[64];
+extern uint8_t flag_rx;
 /* USER CODE END PV */
 
 /** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
@@ -90,10 +92,24 @@
 /** Usb HID report descriptor. */
 __ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DESC_SIZE] __ALIGN_END =
 {
-  /* USER CODE BEGIN 0 */
-  0x00,
-  /* USER CODE END 0 */
-  0xC0    /*     END_COLLECTION	             */
+		/* USER CODE BEGIN 0 */
+		0x06, 0x00, 0xff, // Usage Page(Undefined )
+		0x09, 0x01, // USAGE (Undefined)
+		0xa1, 0x01, // COLLECTION (Application)
+		0x15, 0x00, // LOGICAL_MINIMUM (0)
+		0x26, 0xff, 0x00, // LOGICAL_MAXIMUM (255)
+		0x75, 0x08, // REPORT_SIZE (8)
+		0x95, 0x40, // REPORT_COUNT (64)
+		0x09, 0x01, // USAGE (Undefined)
+		0x81, 0x02, // INPUT (Data,Var,Abs)
+		0x95, 0x40, // REPORT_COUNT (64)
+		0x09, 0x01, // USAGE (Undefined)
+		0x91, 0x02, // OUTPUT (Data,Var,Abs)
+		0x95, 0x01, // REPORT_COUNT (1)
+		0x09, 0x01, // USAGE (Undefined)
+		0xb1, 0x02, // FEATURE (Data,Var,Abs)
+		/* USER CODE END 0 */
+		0xC0 /* END_COLLECTION */
 };
 
 /* USER CODE BEGIN PRIVATE_VARIABLES */
@@ -124,7 +140,8 @@ extern USBD_HandleTypeDef hUsbDeviceFS;
 
 static int8_t CUSTOM_HID_Init_FS(void);
 static int8_t CUSTOM_HID_DeInit_FS(void);
-static int8_t CUSTOM_HID_OutEvent_FS(uint8_t event_idx, uint8_t state);
+static int8_t CUSTOM_HID_OutEvent_FS(uint8_t* state);
+//static int8_t CUSTOM_HID_OutEvent_FS (uint8_t event_idx, uint8_t state);
 
 /**
   * @}
@@ -173,9 +190,13 @@ static int8_t CUSTOM_HID_DeInit_FS(void)
   * @param  state: Event state
   * @retval USBD_OK if all operations are OK else USBD_FAIL
   */
-static int8_t CUSTOM_HID_OutEvent_FS(uint8_t event_idx, uint8_t state)
+static int8_t CUSTOM_HID_OutEvent_FS(uint8_t* state)
+//static int8_t CUSTOM_HID_OutEvent_FS  (uint8_t event_idx, uint8_t state)
 {
   /* USER CODE BEGIN 6 */
+	memcpy(report_buffer, state, 64);
+	USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, report_buffer, 0x40);
+	flag_rx = 1;
   return (USBD_OK);
   /* USER CODE END 6 */
 }
