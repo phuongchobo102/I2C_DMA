0b34232931630cced7214c765d19e56c68f38bf0
Thái Anh <lathaiminh@gmail.com>
2023-02-21 15:59:11 +0700

    Change name of Manufacture to eLinkgate

--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -64,9 +64,9 @@
 
 #define USBD_VID     1155
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "Nova"
+#define USBD_MANUFACTURER_STRING     "eLinkgate"
 #define USBD_PID_FS     22352
-#define USBD_PRODUCT_STRING_FS     "Nova Custom Human interface"
+#define USBD_PRODUCT_STRING_FS     "eLinkgate Custom Human interface"
 #define USBD_CONFIGURATION_STRING_FS     "Custom HID Config"
 #define USBD_INTERFACE_STRING_FS     "Custom HID Interface"
 
